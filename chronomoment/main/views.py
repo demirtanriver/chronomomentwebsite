3d96aadc982a2c7697ab056e840ed23fcfe1d2df
demirtanriver <63473915+demirtanriver@users.noreply.github.com>
2025-06-12 15:29:17 +0100

    added html and set up a base template

--- chronomoment/main/views.py
+++ chronomoment/main/views.py
@@ -5,5 +5,11 @@ from .models import Stories
 
 def index(response,id):
     st = Stories.objects.get(id=id)
-    return HttpResponse("<h1>%s</h1>"%st.main_message)
+    return render(response, "main/story.html",{"st":st})
 
+def home(response):
+    return render(response, "main/home.html",{})
+
+def story(response,id):
+    st = Stories.objects.get(id=id)
+    return render(response, "main/story.html",{"st":st})
